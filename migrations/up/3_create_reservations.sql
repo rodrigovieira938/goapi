bae6769d6f08745e92ffd019d53cb7ea094bb841
Rodrigo Vieira <rodizzvieira@gmail.com>
2025-09-26 09:45:58 +0100

    Change start_date and end_date to TIMESTAMPS

--- migrations/up/3_create_reservations.sql
+++ migrations/up/3_create_reservations.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS reservation (
     id         SERIAL PRIMARY KEY,
     user_id    INT NOT NULL,
     car_id     INT NOT NULL,
-    start_date DATE NOT NULL,
-    end_date   DATE NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date   TIMESTAMP NOT NULL,
     CONSTRAINT fk_user
         FOREIGN KEY(user_id)
         REFERENCES "user"(id)
